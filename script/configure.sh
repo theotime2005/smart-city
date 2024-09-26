d8e256ce55e713d18e7c2122371bf329ee2102d8
theotime2005 <theotime.berthod@epitech.digital>
2024-09-26 23:34:45 +0200

    feat: Add strapi environment and create a script to configure project

---
+++ script/configure.sh
@@ -0,0 +1,21 @@
+echo "Bienvenu dans le script de configuration de l'application"
+# Configure application
+cd ./app
+npm install
+
+echo "Voulez-vous vous connecter à expo? (Y / N)"
+read response
+if [ $response == "y" ]; then
+    npx expo login
+fi
+cd ../
+
+# Configuration de l'API
+cd ./api
+echo "Configuration de l'API"
+cp .env.example .env
+npm install
+npm run build
+
+cd ../
+echo "Configuration terminée"
